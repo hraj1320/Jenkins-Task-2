b38018e4845af8ef82312f53ac052e090e47ca2f
EC2 Default User <ec2-user@ip-172-31-18-1.ec2.internal>
2025-03-11 12:16:22 +0000

    Updated build.sh for static website deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-echo "Starting the Build Process..."
-echo "Build Completed Successfully!" > build_log.txt
-echo "Build log saved."
+
+echo "Starting the Deployment Process..."
+
+# Apache install karein agar pehle install nahi hai
+sudo yum update -y
+sudo yum install -y httpd
+
+# Index.html ko Apache root directory me copy karein
+sudo cp -r /home/ec2-user/TESTING/* /var/www/html/
+
+# Apache server start karein
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+echo "Deployment Successful! Website is now live."
+
